5b74adbacd96eac0a778edb7d540da56cf42a99b
Nguyen Truong Long <longnguyentruong1999@gmail.com>
2024-02-20 14:50:35 +0700

    pagination

--- js/main.js
+++ js/main.js
@@ -7,20 +7,28 @@ const had_favorite = "fa-solid";
 const pink = "pink";
 
 const base_API = "https://pokeapi.co/api/v2/";
-const limit = 12;
+const limit = 30;
 const offset = 0;
 const template = document.querySelector("#template");
 const container = document.querySelector("#container");
 const type = document.querySelector("#type");
 
+var count = 0;
+const pagination = document.querySelector("#pagination");
+const first_btn = '<<';
+const previous_btn = '<';
+const next_btn = '>';
+const last_btn = '>>';
+
 // create Pokemon card
-const createCard = function (name, image) {
+const createCard = function (name, image, type) {
     let content = template.content.querySelector(".card");
     let clone = document.importNode(content, true);
 
     clone.querySelector("span").textContent = name;
     clone.querySelector("img").src = image;
     clone.querySelector("img").alt = name;
+    clone.classList.add(type);
     container.appendChild(clone);
 
     clone.addEventListener("click", function () {
@@ -34,31 +42,62 @@ const fetchPokemonData = function (url) {
     fetch(url).then(function (response) {
         return response.json();
     }).then(function (data) {
-        // console.log(data);
-        createCard(data.name, data.sprites.front_default);
+        console.log(data.id);
+        createCard(data.name, data.sprites.front_default, data.types[0].type.name);
     }).catch(function (error) {
         console.log(error);
     });
 };
 
+// create pagination btn
+const createPaginate = function (i) {
+    let btn = document.createElement("button");
+    let content = document.createTextNode(i);
+    btn.appendChild(content);
+    // btn.value = i;
+
+    btn.addEventListener('click', () => {
+        // console.log(btn.innerHTML);
+        //clear all Pokemon in screen
+        container.innerHTML = '';
+        let n = limit * (parseInt(btn.innerHTML, 10) - 1);
+        console.log(n);
+        fetchPokemonList(limit, n);
+    });
+
+    pagination.appendChild(btn);
+}
+
 // fetch list Pokemom data
-const fetchPokemonList = function () {
-    let link = base_API + 'pokemon?limit=' + limit + '&' + offset + '=' + offset;
+const fetchPokemonList = function (limit, offset) {
+    let link = base_API + 'pokemon?limit=' + limit + '&offset' + '=' + offset;console.log(link);
     fetch(link).then(function (response) {
         // console.log(response);
         return response.json();
     }).then(function (data) {
         let arr = data.results;
+
         arr.forEach(function (e) {
-            // console.log(e);
+            // console.log(e.url);
             fetchPokemonData(e.url);
         });
+
+        // create page btn
+        pagination.innerHTML = '';
+        createPaginate(first_btn);
+        createPaginate(previous_btn);
+        count = Math.ceil(data.count / limit);
+        for (let i = 0; i < count; i++) {
+            createPaginate(i + 1);
+        }
+        createPaginate(next_btn);
+        createPaginate(last_btn);
     }).catch(function (error) {
         console.log(error);
     });
 };
 
-fetchPokemonList();
+fetchPokemonList(limit, offset);
 
 // create type button
 const createTypeBtn = function (name) {
@@ -112,3 +151,5 @@ btn_menu.addEventListener("click", function () {
 //         e.classList.toggle(pink);
 //     });
 // })
+
+// pagination
